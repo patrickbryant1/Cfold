3510cb7be9bf96242f889837075c185fabda1719
Patrick Bryant <patrick.bryant@live.com>
2023-09-06 15:35:49 +0200

    animate

--- src/animate_py3dmol.py
+++ src/animate_py3dmol.py
@@ -1,3 +1,22 @@
+import py3Dmol
+
+import sys
+import os
+import pdb
+
+import pandas as pd
+import numpy as np
+import argparse
+
+
+parser = argparse.ArgumentParser(description = '''Visualise sampled conformations''')
+parser.add_argument('--pdbdir', nargs=1, type= str, required=True, help = "Path to directory with PDB files")
+parser.add_argument('--order_df', nargs=1, type= str, required=True, help = "Path to csv with order of PDB files")
+parser.add_argument('--outdir', nargs=1, type= str, help = 'Outdir.')
+
+
+#################FUNCTIONS#################
+
 class Atom(dict):
     def __init__(self, line):
         self["type"] = line[0:6].strip()
@@ -38,28 +57,37 @@ class Molecule(list):
         return outstr
 
 
-# import py3Dmol
-# molecules = []
-# for ind,row in df.iterrows():
-#     name = row.id+'.pdb'
-#     print(name)
-#     with open(name) as ifile:
-#         molecules.append(Molecule(ifile))
-#
-#
-# view = py3Dmol.view(width=400, height=300)
-#
-# models = ""
-# for i, mol in enumerate(molecules):
-#     models += "MODEL " + str(i) + "\n"
-#     models += str(mol)
-#     models += "ENDMDL\n"
-# view.addModelsAsFrames(models)
-#
-# for i, at in enumerate(molecules[0]):
-#     default = {"cartoon": {'color': 'grey'}}
-#     view.setStyle({'model': -1, 'serial': i+1}, at.get("pymol", default))
-#
-# view.zoomTo()
-# view.animate({'loop': 'backAndForth'})
-# view.show()
+
+
+
+#################MAIN####################
+
+#Parse args
+args = parser.parse_args()
+#Get data
+pdbdir = args.pdbdir[0]
+order_df = pd.read_csv(args.order_df[0]).loc[:10]
+outdir = args.outdir[0]
+
+molecules = []
+for ind,row in order_df.iterrows():
+    with open(pdbdir+row['name']) as ifile:
+        molecules.append(Molecule(ifile))
+
+
+view = py3Dmol.view(width=400, height=300)
+
+models = ""
+for i, mol in enumerate(molecules):
+    models += "MODEL " + str(i) + "\n"
+    models += str(mol)
+    models += "ENDMDL\n"
+view.addModelsAsFrames(models)
+
+for i, at in enumerate(molecules[0]):
+    default = {"cartoon": {'color': 'grey'}}
+    view.setStyle({'model': -1, 'serial': i+1}, at.get("pymol", default))
+
+view.zoomTo()
+view.animate({'loop': 'backAndForth'})
+view.show()
