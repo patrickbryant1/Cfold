808901938944cc52f47f8d1553db6b951d5ede20
Patrick <patrick.bryant@live.com>
2024-01-16 11:35:04 +0100

    installation update

---
+++ install_dependencies.sh
@@ -0,0 +1,20 @@
+
+
+
+## Get network parameters for Cfold
+wget https://zenodo.org/records/10517910/files/params10000.npy
+
+
+## Uniclust30
+wget http://wwwuser.gwdg.de/~compbiol/uniclust/2018_08/uniclust30_2018_08_hhsuite.tar.gz --no-check-certificate
+mkdir data/uniclust30
+mv uniclust30_2018_08_hhsuite.tar.gz data/uniclust30
+tar -zxvf data/uniclust30/uniclust30_2018_08_hhsuite.tar.gz
+
+
+## HHblits
+git clone https://github.com/soedinglab/hh-suite.git
+mkdir -p hh-suite/build && cd hh-suite/build
+cmake -DCMAKE_INSTALL_PREFIX=. ..
+make -j 4 && make install
+cd ..
